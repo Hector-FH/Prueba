ab2d476d4e5568df95a57ad473cc2a5469fb4433
Hector-FH <hhachahector10@gmail.com>
2025-04-24 19:25:04 -0600

    Prueba1

---
+++ src/main/java/Pruebas/Prueba1.java
@@ -0,0 +1,29 @@
+package Pruebas;
+
+import java.util.Scanner;
+
+public class Prueba1 {
+
+
+    public static void main (String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+
+        String nombre = "";
+
+        System.out.println("Ingresa el nombre");
+        nombre = sc.nextLine( );
+
+        System.out.println( nombre );
+
+
+    }
+
+
+
+
+
+
+
+}
